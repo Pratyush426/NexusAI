7bb806dfcf047dc1663181bf6c439f08e71fd312
Pratyush <pratyushmohanty2912@gmail.com>
2026-01-24 01:14:46 +0530

    Updated

--- server/config/db.js
+++ server/config/db.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/jobtrack");
-console.log("Connected to DB:", mongoose.connection.name);
+    const conn = await mongoose.connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/jobtrack");
+    console.log("Connected to DB:", conn.connection.name);
   } catch (err) {
     console.error(err);
   }
